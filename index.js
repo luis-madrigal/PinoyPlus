bfb37f02eeae3e37bb02cbc8370c882a8d66abfd
JOple <jose_jaena_ople@dlsu.edu.ph>
2018-08-06 20:47:43 +0800

    Returned the config for non-localhost testing

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const config = {
 
     port: process.env.port || process.env.PORT || 3000,
     adminAccount: "admin@pinoyplus",
-    // adminServerUrl: "http://104.215.190.53:9000/",
-    // chatServerUrl: "http://104.215.190.53:4000/",
-    adminServerUrl: "http://localhost:5280/api/",
-    chatServerUrl: "http://localhost:5280/bosh/",
+    adminServerUrl: "http://104.215.190.53:9000/",
+    chatServerUrl: "http://104.215.190.53:4000/",
+    // adminServerUrl: "http://localhost:5280/api/",
+    // chatServerUrl: "http://localhost:5280/bosh/",
     chatHost: "pinoyplus",
     sessionSecret: "asdpfodkdpvk134po1kp24okfpokd-v0ss-wmwkemld",
 }
