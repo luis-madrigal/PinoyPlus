25af1d7912185b11c297ece60dd9df92bd81a42e
Richmond Manga <chinomanga@outlook.com>
2018-08-17 01:25:07 +0800

    added return button to chat box

--- public/js/messagebox.js
+++ public/js/messagebox.js
@@ -5,9 +5,15 @@ function w3_close() {
     $('#chatBox').css('display', 'none');
 }
 
+
+
 $(document).ready(function () {
     $('.contact-item').click(function (e) {
         $('.chat-list-container').css('display', 'none');
         $('.main-chat-container').css('display', 'block');
+    });
+    $('#contact-btn').click(function (e) {
+        $('.chat-list-container').css('display', 'block');
+        $('.main-chat-container').css('display', 'none');
     });
 });
